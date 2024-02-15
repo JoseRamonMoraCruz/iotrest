7d6fa0583edf1ac30ecc8754f99b0d2462f901d7
Jose Ramon Mora Cruz <144741149+JoseRamonMoraCruz@users.noreply.github.com>
2024-02-15 15:00:09 -0600

    commit

---
+++ database/migrations/2024_02_15_144336_create_sensors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->id();
+            $table->string("type");//tipo de sensor
+            $table->decimal("value", 10, 2);//valor del sensor
+            $table->datetime("date");//fecha y hora
+            $table->string("user");//usuario qeu modifico datos
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sensors');
+    }
+};
